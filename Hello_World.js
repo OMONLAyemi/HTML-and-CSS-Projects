e4b3784296518d7db130b3455c87af68bcc7718c
OMONLAyemi <125382350+OMONLAyemi@users.noreply.github.com>
2023-03-13 13:00:49 -0500

    practice basic Css

---
+++ Hello_World.js
@@ -0,0 +1,8 @@
+var greetings = 'Hello, World!'
+    console.log(greetings);
+
+var greeting2 = 'Welcome to Node.js'
+console.log(greetings);
+
+var sentence1 = 'Learning how to debug code with the debugger';
+console.log(sentence);
